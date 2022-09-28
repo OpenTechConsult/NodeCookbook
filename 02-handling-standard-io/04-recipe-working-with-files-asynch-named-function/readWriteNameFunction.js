c04f0b1c005d5f5e2a685de9742788076e494415
OpenTech-Consult <opentech.consult@gmail.com>
2022-09-28 09:48:56 +0000

    Demonstrated that readWriteNameFunction.js is asynchronous code
    Added the setInterval() function to print a string every 1 milliseconds

--- 02-handling-standard-io/04-recipe-working-with-files-asynch-named-function/readWriteNameFunction.js
+++ 02-handling-standard-io/04-recipe-working-with-files-asynch-named-function/readWriteNameFunction.js
@@ -2,14 +2,7 @@ import { readFile, writeFile } from 'node:fs'
 import { join } from 'node:path'
 
 const filePath = join(process.cwd(), "hello.txt")
-readFile(filePath, "utf8", (err, contents) => {
-    if (err) {
-        return console.error(err)
-    }
-    console.log('File contents:', contents)
-    const upperContents = contents.toUpperCase()
-    updateFile(filePath, upperContents)
-})
+
 
 function updateFile(filepath, contents) {
     writeFile(filepath, contents, (err) => {
@@ -19,3 +12,15 @@ function updateFile(filepath, contents) {
         console.log('File updated')
     })
 }
+
+
+readFile(filePath, "utf8", (err, contents) => {
+    if (err) {
+        return console.error(err)
+    }
+    console.log('File contents:', contents)
+    const upperContents = contents.toUpperCase()
+    updateFile(filePath, upperContents)
+})
+
+setInterval(() => process.stdout.write('******\n'), 1).unref()
