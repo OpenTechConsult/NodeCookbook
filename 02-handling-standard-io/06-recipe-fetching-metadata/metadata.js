2386e6565be0192d6010706662f7b17647dbaf43
OpenTech-Consult <opentech.consult@gmail.com>
2022-09-28 15:30:58 +0000

    wrap the logic with a try catch block to gracefully handle exceptions

--- 02-handling-standard-io/06-recipe-fetching-metadata/metadata.js
+++ 02-handling-standard-io/06-recipe-fetching-metadata/metadata.js
@@ -4,8 +4,12 @@ const file = process.argv[2]
 
 // create the printMetaData to get the file info
 function printMetaData(file) {
-    const fileStats = statSync(file)
-    console.log(fileStats)
+    try {
+        const fileStats = statSync(file)
+        console.log(fileStats)
+    } catch (err) {
+        console.error('Error readind the file path:', file)
+    }
 }
 
 // call the printMetaData function
