54d849dc1639c6533db90bc97f1622055ff692f3
OpenTech-Consult <opentech.consult@gmail.com>
2022-09-28 22:49:28 +0000

    Remove the constants used to create the numeric bitmask.
    Instead we used the octal representation of file permissions.

--- 02-handling-standard-io/07-recipe-modif-file-permission/change-permission.js
+++ 02-handling-standard-io/07-recipe-modif-file-permission/change-permission.js
@@ -1,9 +1,4 @@
 import { chmodSync, constants } from 'node:fs'
 
 const file = './file.txt'
-chmodSync(file,
-    constants.S_IRUSR |
-    constants.S_IWUSR |
-    constants.S_IRGRP |
-    constants.S_IWGRP |
-    constants.S_IROTH)
+chmodSync(file, 0o664)
