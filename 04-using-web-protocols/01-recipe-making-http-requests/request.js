c1db22a02e24f581a795a35a737795fd8ac01ba2
OpenTech-Consult <opentech.consult@gmail.com>
2022-10-03 23:29:36 +0000

    We've seen how to use Node.js http core module to send HTTP GET request and HTTP POST requests.

--- 04-using-web-protocols/01-recipe-making-http-requests/request.js
+++ 04-using-web-protocols/01-recipe-making-http-requests/request.js
@@ -1,7 +1,39 @@
-import { get } from 'node:http'
+import { get, request } from 'node:http'
 
 // send a GET request to http://example.com
-get('http://example.com', (res) => {
+// get('http://example.com', (res) => {
+//     res.pipe(process.stdout)
+// })
+
+// data to  be send with the HTTP POST request
+const payload = `{
+    "name": "Beth",
+    "job": "Software Engineer"
+}`
+
+const opts = {
+    method: "POST",
+    hostname: "postman-echo.com",
+    path: "/post",
+    headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(payload),
+    },
+}
+
+
+// send HTTP request
+const req = request(opts, (res) => {
+    process.stdout.write('Status Code: ' + res.statusCode + '\n')
+    process.stdout.write('Body: ')
     res.pipe(process.stdout)
 })
 
+
+// catch any error that occurred on the requests.
+req.on('error', (err) => {
+    console.error('Error: ' + err)
+})
+
+// send the request with the payload
+req.end(payload)
