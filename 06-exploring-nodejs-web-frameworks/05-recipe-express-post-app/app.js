5687940408a12e76d8dda9be1a990b7a212f7872
OpenTech-Consult <opentech.consult@gmail.com>
2022-10-18 18:57:58 +0000

    Handling POST requests and route parameters
    created a form and use the body parser module to send data to the web server. Also treat the route parameters

---
+++ 06-exploring-nodejs-web-frameworks/05-recipe-express-post-app/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const index = require('./routes/index')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
+app.use('/', index)
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`)
+})
